7fb5e95d0c68393eb2144d98aa37b1e6743437b0
N-Plus-Plus <troy.nissen@gmail.com>
2022-01-21 14:22:45 +1100

    Add files via upload

--- code.js
+++ code.js
@@ -11,11 +11,24 @@ let game = {
     , currentRow: 0
     , over: false
     , success: null
+    , diff: 5
 };
 let stats = {
-    guesses: 0
-    , lost: 0
-    , won: [0,0,0,0,0,0]
+    diff4: {
+        guesses: 0
+        , lost: 0
+        , won: [0,0,0,0,0]
+    }
+    , diff5: {
+        guesses: 0
+        , lost: 0
+        , won: [0,0,0,0,0,0]
+    }
+    , diff6: {
+        guesses: 0
+        , lost: 0
+        , won: [0,0,0,0,0,0,0]
+    }
 }
 
 function onLoad(){
@@ -35,17 +48,17 @@ function newGame(){
             }
         }
     }
-    for( let i = 0; i <= 6; i++ ){ game.board[`r${i}`] = { input: [], locked: false } }
+    for( let i = 0; i <= game.diff + 1; i++ ){ game.board[`r${i}`] = { input: [], locked: false } }
     game.currentRow = 0;
     game.over = false;
     game.success = null;    
-    buildMain();    
+    buildMain();
     buildKeyboard();
 }
 
 function pickWord(){
-    let n = Math.floor( Math.random() * common.length );
-    game.word = common[n];
+    let n = Math.floor( Math.random() * dict[`diff${game.diff}`].choice.length );
+    game.word = dict[`diff${game.diff}`].choice[n].toUpperCase();
 }
 
 function buildKeyboard(){
@@ -71,10 +84,10 @@ function buildKeyboard(){
 function buildMain(){
     let target = document.getElementById(`mainPanel`);
     target.innerHTML = ``;
-    for( let i = 0; i < 6; i++ ){
+    for( let i = 0; i < game.diff + 1; i++ ){
         let row = document.createElement(`div`);
         row.classList = `guessRow flexy`;
-        for( let j = 0; j < 5; j++ ){
+        for( let j = 0; j < game.diff; j++ ){
             let cell = document.createElement(`div`);
             cell.classList = `cell bounce blank flexy`;
             row.appendChild( cell );
@@ -114,7 +127,7 @@ function clicked( e ){
 }
 function type( q ){
     if( !game.over ){
-        if( game.board[`r${game.currentRow}`].input.length < 5 ){
+        if( game.board[`r${game.currentRow}`].input.length < game.diff ){
             game.board[`r${game.currentRow}`].input.push( q );
             let cell = document.querySelector(`#mainPanel`).children[game.currentRow].children[game.board[`r${game.currentRow}`].input.length-1];
             cell.innerHTML = q;
@@ -128,15 +141,15 @@ function type( q ){
 function updateBoard(){
     let target = document.getElementById(`mainPanel`);
     target.innerHTML = ``;
-    for( let i = 0; i < 6; i++ ){
+    for( let i = 0; i < game.diff + 1; i++ ){
         let row = document.createElement(`div`);
         row.classList = `guessRow flexy`;
-        for( let j = 0; j < 5; j++ ){
+        for( let j = 0; j < game.diff; j++ ){
             let cell = document.createElement(`div`);
             cell.classList = `cell bounce blank flexy`;
             if( game.board[`r${i}`].input[j] !== undefined ){ 
                 if( game.currentRow > i ){
-                    cell.classList = `cell flexy ${testLetter( game.board[`r${i}`].input[j], j )}`;
+                    cell.classList = `cell flexy ${testLetter( game.board[`r${i}`].input[j], j, game.currentRow )}`;
                 }
                 cell.innerHTML = game.board[`r${i}`].input[j];
             }
@@ -149,20 +162,20 @@ function updateBoard(){
 function progressRow(){
     let c = document.getElementById(`mainPanel`).children[game.currentRow];
     for( let i = 0; i < c.children.length; i++ ){
-        c.children[i].classList.add( testLetter( game.board[`r${game.currentRow}`].input[i], i ) );
+        c.children[i].classList.add( testLetter( game.board[`r${game.currentRow}`].input[i], i, game.currentRow ) );
     }
     game.currentRow++;
 }
 
-function testLetter( q, i ){
+function testLetter( q, i, r ){
     if( game.word[i] == q ){ return `right`; }
     else if( game.word.indexOf( q ) != -1 ){
         // if they've all been guessed already, locked
         let count = game.word.split(q).length - 1;
         let acccount = 0;
-        for( let n = 0; n < 5; n++ ){
+        for( let n = 0; n < game.diff; n++ ){
             if( n == i ){}
-            else if( game.board[`r${game.currentRow}`].input[n] == q && game.word[n] == q ){ acccount++; }
+            else if( game.board[`r${r}`].input[n] == q && game.word[n] == q ){ acccount++; }
         }
         if( count == acccount ){ return `locked` }
         // otherwsie
@@ -182,11 +195,11 @@ function updateKeyboard(){
 }
 
 function submitGuess(){
-    if( game.board[`r${game.currentRow}`].input.length == 5 ){
-        stats.guesses++;
+    if( game.board[`r${game.currentRow}`].input.length == game.diff ){
+        stats[`diff${game.diff}`].guesses++;
         saveState();
         let whole = game.board[`r${game.currentRow}`].input.join(``);
-        if( full.findIndex( element => element == whole ) == -1 ){
+        if( dict[`diff${game.diff}`].valid.findIndex( element => element == whole ) == -1 ){
             notWord();
         }
         else {
@@ -195,11 +208,11 @@ function submitGuess(){
                 gameOver( game.currentRow );
             }
             else{ 
-                for( let i = 0; i < 5; i++ ){
+                for( let i = 0; i < game.diff; i++ ){
                     let guess = game.board[`r${game.currentRow}`].input[i];
                     game.keys[`${guess}`].guessed = true;
                 }
-                if( game.currentRow >= 5 ){
+                if( game.currentRow >= game.diff ){
                     game.success = false;
                     game.over = true;
                     gameOver(game.currentRow);
@@ -212,8 +225,8 @@ function submitGuess(){
 }
 
 function gameOver( r ){
-    if( game.success ){ stats.won[r]++; }
-    else if( !game.success ){ stats.lost++; }    
+    if( game.success ){ stats[`diff${game.diff}`].won[r]++; }
+    else if( !game.success ){ stats[`diff${game.diff}`].lost++; }    
     saveState();
     generateStats();
     toggleModal( game.success );
@@ -255,16 +268,16 @@ function generateStats(){
     canvS.width = document.body.clientHeight * 0.4;
     canvS.height = document.body.clientHeight * 0.4;
     ctxS.clearRect( 0, 0, document.body.clientHeight * 0.4, document.body.clientHeight * 0.4 );
-    let cell = document.body.clientHeight * 0.4 / 7;
-    let gap = document.body.clientHeight * 0.4 / ( cell * 6 );
+    let cell = document.body.clientHeight * 0.4 / ( game.diff + 1 );
+    let gap = document.body.clientHeight * 0.4 / ( cell * ( game.diff + 1 ) ) / 2;
     ctxS.strokeStyle = `#111`;
     ctxS.lineWidth = 2;
-    let colLookup = { right: `#279277`, wrong: `#8067B7`, locked: `#444` }
-    for( let i = 0; i < 6; i++ ){
+    let colLookup = { right: `#279277`, wrong: `#b39037`, locked: `#444` }
+    for( let i = 0; i < game.diff + 1; i++ ){
         let y = gap + cell * i + gap * i;
-        for( let j = 0; j < 5; j++ ){
+        for( let j = 0; j < game.diff; j++ ){
             // get the colour
-            let colour = testLetter( game.board[`r${i}`].input[j], j );            
+            let colour = testLetter( game.board[`r${i}`].input[j], j, i );
             let x = ( gap * 2 + cell ) + cell * j + gap * j;
             ctxS.beginPath();
             ctxS.rect( x, y, cell, cell )
@@ -275,22 +288,22 @@ function generateStats(){
         }
     }
     // historical overview
-    let watermark = Math.max( 1, stats.lost );
-    for( k in stats.won ){ if( stats.won[k] > watermark ){ watermark = stats.won[k] }; };
+    let watermark = Math.max( 1, stats[`diff${game.diff}`].lost );
+    for( k in stats[`diff${game.diff}`].won ){ if( stats[`diff${game.diff}`].won[k] > watermark ){ watermark = stats[`diff${game.diff}`].won[k] }; };
     let target = document.getElementById(`stats`);
     target.innerHTML = ``;
     let e1 = document.createElement(`div`);
     e1.classList = `statsRow`;
-    e1.innerHTML = `<div class="statsLeft">Guesses:</div><div class="statNum">${stats.guesses}</div>`;
+    e1.innerHTML = `<div class="statsLeft">Guesses:</div><div class="statNum">${stats[`diff${game.diff}`].guesses}</div>`;
     target.appendChild( e1 );
     let e2 = document.createElement(`div`);
     e2.classList = `statsRow`;
-    e2.innerHTML = `<div class="statsLeft">Losses:</div><div class="statsRight">${stats.lost}</div><div class="statBar"><div class="barFill" style="width: ${ stats.lost / watermark * 100 }%;"></div></div>`
+    e2.innerHTML = `<div class="statsLeft">Losses:</div><div class="statsRight">${stats[`diff${game.diff}`].lost}</div><div class="statBar"><div class="barFill" style="width: ${ stats[`diff${game.diff}`].lost / watermark * 100 }%;"></div></div>`
     target.appendChild( e2 );
-    for( let i = 0; i < 6; i++ ){
+    for( let i = 0; i < game.diff + 1; i++ ){
         let e = document.createElement(`div`);
         e.classList = `statsRow`;
-        e.innerHTML = `<div class="statsLeft">Turn ${i + 1}:</div><div class="statsRight">${stats.won[i]}</div><div class="statBar"><div class="barFill" style="width: ${ stats.won[i] / watermark * 100 }%;"></div></div>`;
+        e.innerHTML = `<div class="statsLeft">Turn ${i + 1}:</div><div class="statsRight">${stats[`diff${game.diff}`].won[i]}</div><div class="statBar"><div class="barFill" style="width: ${ stats[`diff${game.diff}`].won[i] / watermark * 100 }%;"></div></div>`;
         target.appendChild( e );
     }
 }
